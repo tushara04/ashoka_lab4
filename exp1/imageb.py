f87432db60eb982ef77b0c22c48717f857fb6634
tushara04 <tusharagarwal1016@gmail.com>
2025-09-07 20:49:30 +0530

    updated

--- exp1/imageb.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-
-img = cv2.imread("/home/tushara/Documents/Ashoka/Lab 4/Data/exp1/thickness_measurement.png", cv2.IMREAD_GRAYSCALE)
-
-plt.imshow(img)
-plt.show()
